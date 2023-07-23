04f44482636ea03a8c1c1a6dd28f755f2c310366
Abhijit <abhijitdarade.vd@gmail.com>
2023-07-23 07:08:14 +0530

    Link Change

--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -77,7 +77,7 @@ function Home2() {
               </li>
               <li className="social-icons">
                 <a
-                  href="https://linkedin.com/in/abhijit-darade/"
+                  href="https://www.linkedin.com/in/abhijit-darade-32a293208/"
                   target="_blank"
                   rel="noreferrer"
                   className="icon-colour  home-social-icons"
